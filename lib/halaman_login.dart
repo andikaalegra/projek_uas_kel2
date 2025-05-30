24fcd2aa9ab489e0d053c0f77422debc4d78aa49
andikaalegra <andikadikadika37@gmail.com>
2025-05-30 19:40:23 +0700

    Menambahkan halaman login

---
+++ lib/halaman_login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HalamanLogin extends StatelessWidget {
+  const HalamanLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
