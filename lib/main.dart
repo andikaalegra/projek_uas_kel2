9124df633166cc88a06cc841be17ca456e5c2585
andikaalegra <andikadikadika37@gmail.com>
2025-06-03 13:42:43 +0700

    Integrasi Firebase ke Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:projek_uas_kel2/firebase_options.dart';
 import 'halaman_login.dart';
 
-void main() {
+void main() async{
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const MyApp());
 }
 
