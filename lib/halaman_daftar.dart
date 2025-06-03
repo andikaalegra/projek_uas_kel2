630a51aa988f6dbbaf4e23d612be13afa71c48c0
Bellaam30 <bellaamandam30@gmail.com>
2025-06-03 13:55:13 +0700

    menambahkan daftar

---
+++ lib/halaman_daftar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HalamanDaftar extends StatefulWidget {
+  const HalamanDaftar({super.key});
+
+  @override
+  State<HalamanDaftar> createState() => _HalamanDaftarState();
+}
+
+class _HalamanDaftarState extends State<HalamanDaftar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
