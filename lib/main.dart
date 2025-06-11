8eee89c61302f67c3009d2037160b800d4f004c2
andikaalegra <andikadikadika37@gmail.com>
2025-06-11 11:08:11 +0700

    Selesai resolve konflik merge coba-auth ke main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:projek_uas_kel2/daftar_nasabah.dart';
+import 'package:projek_uas_kel2/admin/daftar_nasabah.dart';
 import 'package:projek_uas_kel2/firebase_options.dart';
 import 'halaman_login.dart';
 import 'halaman_daftar.dart';
-import 'dashboard_admin.dart';
-import 'dashboard_nasabah.dart';
+import 'admin/dashboard_admin.dart';
+import 'nasabah/dashboard_nasabah.dart';
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
@@ -28,11 +27,7 @@ class MyApp extends StatelessWidget {
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-<<<<<<< HEAD
-      home: HalamanLogin(),
-=======
       home:HalamanLogin(),
->>>>>>> b360c1666d8c4296cd1b68356653896fb4eef387
     );
   }
 }
