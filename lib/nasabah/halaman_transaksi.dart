1bca56dda7d71b7181a6eb7bdd8d6db09ea9ce1a
andikaalegra <andikadikadika37@gmail.com>
2025-06-11 14:48:33 +0700

    halaman transaksi

---
+++ lib/nasabah/halaman_transaksi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HalamanTransaksi extends StatefulWidget {
+  const HalamanTransaksi({super.key});
+
+  @override
+  State<HalamanTransaksi> createState() => _HalamanTransaksiState();
+}
+
+class _HalamanTransaksiState extends State<HalamanTransaksi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
